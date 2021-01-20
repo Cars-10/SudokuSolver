639113b77b4160e623adcc6ec76a6c0b1021c314
Cars10 <clawrenz@gmail.com>
2021-01-20 09:08:48 +0100

    Added DEBUG controlled print statements

--- C/Sudoku.c
+++ C/Sudoku.c
@@ -5,6 +5,7 @@
 // in C Array indexes are [j][i] [row][col]!
 int puzzle[9][9];
 int count;
+int DEBUG = 0; //0 off, 1 High Level, 3 Low Level
 
 void printPuzzle() {
     printf("\nPuzzle:\n");
@@ -39,8 +40,8 @@ int readMatrixFile(char * filename ) {
         if(line_buf[0] != '#') 
         {
             /* Show the line details */
-            //printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_count,
-            // line_size, line_buf_size, line_buf);
+            if (DEBUG==3) printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_count,
+              line_size, line_buf_size, line_buf);
 
             if (line_buf_size == 32)
             {
@@ -48,8 +49,10 @@ int readMatrixFile(char * filename ) {
                 &puzzle[line_count][2], &puzzle[line_count][3], &puzzle[line_count][4], &puzzle[line_count][5], 
                 &puzzle[line_count][6], &puzzle[line_count][7], &puzzle[line_count][8] 
                 );
-                //for (int i = 0; i < 10; i++) printf("%i " , puzzle[line_count][i]);
-                //printf("\n");
+                if (DEBUG==3) {
+                    for (int i = 0; i < 10; i++) printf("%i " , puzzle[line_count][i]);
+                    printf("\n");
+                }
         
                 /* Increment our line count */
                 line_count++;
@@ -73,7 +76,7 @@ int floor_div(int a, int b) {
 }
 
 int isPossible(int y, int x, int val) {
-    //printf("Is possible %i, %i, %i\n" ,x ,y ,val);
+    if (DEBUG) printf("Is possible %i, %i, %i count=%i\n" ,x ,y ,val, count);
     // Find if a matching number (val) already exists
     // in the same row (y) or column (x) or within its rectangle
     for (int i = 0; i <9; i++) if(puzzle[i][x] == val) return 0; 
@@ -83,27 +86,28 @@ int isPossible(int y, int x, int val) {
     // Find which 3x3 square we are in using the floor quotient
     int x0= (floor_div(x,3))*3;
     int y0= (floor_div(y,3))*3;
-    //printf("Is possible x=%i x0=%i, y=%i y0=%i, val=%i\n" , x, x0, y, y0, val);
+    if (DEBUG) printf("Is possible x=%i x0=%i, y=%i y0=%i, val=%i\n" , x, x0, y, y0, val);
 
     for (int i = 0; i <3; i++)
     {
         for (int j = 0; j <3; j++)
         {
-            //printf("y0+i=%i i=%i, x0+j=%i j=%i Puzzle[y0+i][x0+j]=%i, val=%i\n", y0+i,i, x0+j,j, puzzle[y0+i][x0+j] , val);
+            if (DEBUG) printf("y0+i=%i i=%i, x0+j=%i j=%i Puzzle[y0+i][x0+j]=%i, val=%i\n", y0+i,i, x0+j,j, puzzle[y0+i][x0+j] , val);
             if(puzzle[y0+i][x0+j] == val ) return 0; 
         }
     }
-    //printf("YES possible %i, %i, %i\n" ,x ,y ,val);
+    if (DEBUG) printf("YES possible %i, %i, %i\n" ,x ,y ,val);
     return 1;
 }
 
 int solve() {
     for (int j = 0; j < 9; j++) {
         for (int i = 0; i < 9; i++) {
-            //printf("i=%i,j=%i:%i\n" ,i,j,puzzle[i][j]);
+            if (DEBUG) printf("i=%i,j=%i:%i\n" ,i,j,puzzle[i][j]);
             if (puzzle[j][i] == 0) {
                 for (int val = 1; val < 10; val++) {
                     count += 1;
+                    if (count==117) printPuzzle();
                     if (isPossible(j,i,val) == 1)
                     {
                         puzzle[j][i] = val;
@@ -130,7 +134,7 @@ int main(int argc, char** argv) {
     for (i = 0; i < argc; i++)
     {
         if((point = strrchr(argv[i],'.')) != NULL ) {
-            //printf("Main: i=%i: %s\n", i, argv[i]);
+            if (DEBUG) printf("Main: i=%i: %s\n", i, argv[i]);
             //Check if supplied filename ends with csv
             if(strcmp(point,".matrix") == 0) {
                 printf("%s\n", argv[i]);
