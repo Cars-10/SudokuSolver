10c51103adf9afffa51f2bf9c7ed6f5a7b6f3938
Cars10 <clawrenz@gmail.com>
2021-02-01 12:20:59 +0100

    Fixed Debug printouts to help dbung other versions of the solver

--- C/Sudoku.c
+++ C/Sudoku.c
@@ -28,9 +28,10 @@ int readMatrixFile(char * filename ) {
     file = fopen(filename, "r");
     if (!file)
     {
-      printf(stderr, "Error opening file '%s'\n", filename);
+      fprintf(stderr, "Error opening file '%s'\n", filename);
       return 1;
     }
+    printf("%s\n", filename);
     /* Get the first line of the file. */
     line_size = getline(&line_buf, &line_buf_size, file);
     /* Loop through until we are done with the file. */
@@ -76,7 +77,7 @@ int floor_div(int a, int b) {
 }
 
 int isPossible(int y, int x, int val) {
-    if (DEBUG) printf("Is possible %i, %i, %i count=%i\n" ,y ,x ,val, count);
+    if (DEBUG) printf("Is possible %i, %i, %i\n" ,y ,x ,val);
     // Find if a matching number (val) already exists
     // in the same row (y) or column (x) or within its rectangle
     for (int i = 0; i <9; i++) if(puzzle[i][x] == val) return 0; 
@@ -86,27 +87,27 @@ int isPossible(int y, int x, int val) {
     // Find which 3x3 square we are in using the floor quotient
     int x0= (floor_div(x,3))*3;
     int y0= (floor_div(y,3))*3;
-    if (DEBUG) printf("Is possible x=%i x0=%i, y=%i y0=%i, val=%i\n" , x, x0, y, y0, val);
 
     for (int i = 0; i <3; i++)
     {
         for (int j = 0; j <3; j++)
         {
-            if (DEBUG) printf("y0+i=%i i=%i, x0+j=%i j=%i Puzzle[y0+i][x0+j]=%i, val=%i\n", y0+i,i, x0+j,j, puzzle[y0+i][x0+j] , val);
+            if (DEBUG>1) printf("Is Possible: y0+i=%i i=%i, x0+j=%i j=%i Puzzle[y0+i][x0+j]=%i, val=%i\n", y0+i,i, x0+j,j, puzzle[y0+i][x0+j] , val);
             if(puzzle[y0+i][x0+j] == val ) return 0; 
         }
     }
-    if (DEBUG) printf("YES possible %i, %i, %i\n" ,y ,x ,val);
+    if (DEBUG) printf("YES possible\n");
     return 1;
 }
 
 int solve() {
     for (int j = 0; j < 9; j++) {
         for (int i = 0; i < 9; i++) {
-            if (DEBUG) printf("i=%i,j=%i:%i\n" ,i,j,puzzle[i][j]);
             if (puzzle[j][i] == 0) {
+                if (DEBUG) printf("Solve: j=%i,i=%i: %i\n" ,j,i,puzzle[j][i]);
                 for (int val = 1; val < 10; val++) {
                     count += 1;
+                    if (DEBUG) printf("Count= %i\n",count);
                     if (isPossible(j,i,val) == 1)
                     {
                         puzzle[j][i] = val;
@@ -133,15 +134,14 @@ int main(int argc, char** argv) {
     for (i = 0; i < argc; i++)
     {
         if((point = strrchr(argv[i],'.')) != NULL ) {
-            if (DEBUG) printf("Main: i=%i: %s\n", i, argv[i]);
+            if (DEBUG>1) printf("Main: i=%i: %s\n", i, argv[i]);
             //Check if supplied filename ends with csv
             if(strcmp(point,".matrix") == 0) {
-                printf("%s\n", argv[i]);
                 readMatrixFile(argv[i]);
                 printPuzzle(); 
+                count = 0;
+                solve();
             }
-            count = 0;
-            solve();
         }
     }
     
